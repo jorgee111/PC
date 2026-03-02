998652cccb4a5a6300d3b9c9564a5889b19279fc
unknown <jorgedelapenya@gmail.com>
2026-03-02 20:02:09 +0100

    Config: Added start script and dynamic PORT for Render deployment

--- api-cityflow/app.js
+++ api-cityflow/app.js
@@ -3,13 +3,13 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import usersRoutes from './src/routes/users.routes.js';
-import linesRoutes from './src/routes/lines.routes.js'; 
+import linesRoutes from './src/routes/lines.routes.js';
 import incidentsRoutes from './src/routes/incidents.routes.js';
 import vehiclesRoutes from './src/routes/vehicles.routes.js';
 import { openDB } from './src/db/db.js';
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 openDB();
 
 // Registro de rutas corregido
-app.use('/api/users', usersRoutes); 
-app.use('/api/lines', linesRoutes); 
+app.use('/api/users', usersRoutes);
+app.use('/api/lines', linesRoutes);
 app.use('/api/incidents', incidentsRoutes); // Ruta para la tabla del admin
 app.use('/api/vehicles', vehiclesRoutes);
 
